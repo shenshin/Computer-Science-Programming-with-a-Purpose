c270f9ac2c704c891260272f0eece51120512385
Леонид Широков <leonid@Air-Leonid.home>
2020-06-08 04:01:23 +0200

    Change methods names in RevesPuzzle, add Mandelbrot.java

--- RevesPuzzle.java
+++ RevesPuzzle.java
@@ -7,27 +7,27 @@
 public class RevesPuzzle {
     public static void main(String[] args) {
         int n = Integer.parseInt(args[0]);
-        hanoi(n, "a", "b", "c", "d");
+        reves(n, "a", "b", "c", "d");
     }
 
-    private static void hanoi3(int n, int k, String from, String to,
-                               String temp) {
+    private static void hanoi(int n, int k, String from, String to,
+                              String temp) {
         if (n != 0) {
-            hanoi3(n - 1, k, from, temp, to);
+            hanoi(n - 1, k, from, temp, to);
             print(n + k, from, to);
-            hanoi3(n - 1, k, temp, to, from);
+            hanoi(n - 1, k, temp, to, from);
         }
     }
 
-    private static void hanoi(int n, String from, String temp1,
+    private static void reves(int n, String from, String temp1,
                               String temp2, String to) {
         if (n == 1)
             print(n, from, to);
         else {
             int k = (int) Math.round((double) n + 1 - Math.sqrt(2 * (double) n + 1));
-            hanoi(k, from, temp2, to, temp1);
-            hanoi3(n - k, k, from, to, temp2);
-            hanoi(k, temp1, from, temp2, to);
+            reves(k, from, temp2, to, temp1);
+            hanoi(n - k, k, from, to, temp2);
+            reves(k, temp1, from, temp2, to);
         }
     }
 
